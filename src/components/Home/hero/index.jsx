8d1eafa1fda4cd5f4024472664f00f737f6b473a
mohsindawood <mohsindawood58@gmail.com>
2024-07-25 09:18:48 +0500

    update offered in player profile and responsive in mobile

--- src/components/Home/hero/index.jsx
+++ src/components/Home/hero/index.jsx
@@ -7,28 +7,30 @@ export default function Hero() {
   const navigate = useNavigate();
   return (
     <div
-      className="min-h-screen "
+      className='min-h-screen '
       style={{
         backgroundImage: `url(${footerBg})`,
         backgroundPosition: 'center',
         backgroundSize: 'cover',
-      }}
-    >
-      <div className="max-w-[1440px] mx-auto px-3 lg:px-[20px] min-h-screen flex items-center z-[100]">
-        <div className="flex flex-col gap-2 max-w-[700px]">
-          <h1 className="text-[50px] text-white mb-0">GET RECRUITED</h1>
-          <h1 className="text-[50px] text-primaryColor mb-0">BE DISCOVERED</h1>
+      }}>
+      <div className='max-w-[1440px] mx-auto px-3 lg:px-[20px] min-h-screen flex items-center z-[100]'>
+        <div className='flex flex-col gap-2 max-w-[700px]'>
+          <h1 className='text-[36px] md:text-[50px] text-white mb-0'>
+            GET RECRUITED
+          </h1>
+          <h1 className='text-[36px] md:text-[50px] text-primaryColor mb-0'>
+            BE DISCOVERED
+          </h1>
           <h1
-            className="text-[50px] text-green mb-0"
+            className='text-[36px] md:text-[50px] text-green mb-0'
             style={{
               textShadow:
                 '-1px -1px 0 #fff,1px -1px 0 #fff,-1px 1px 0 #fff,1px 1px 0 #fff ',
-            }}
-          >
+            }}>
             PLAY COLLEGE BASKETBALL
           </h1>
 
-          <p className="text-white font-normal">
+          <p className='text-white font-normal'>
             Our advanced app, Undiscovered Recruits, bridges the gap between
             basketball players and coaches, enhancing the recruitment process.
             Key features include seamless video uploads, direct messaging, and
@@ -37,47 +39,45 @@ export default function Hero() {
             discover their future, while coaches can discover top talent and
             streamline their recruitment efforts.
           </p>
-          <div className="flex items-center gap-2 flex-row mt-4">
+          <div className='flex items-center gap-2 flex-col md:flex-row mt-4'>
             <button
-              className="flex items-center gap-2 text-sm leading-6 bg-primaryColor py-2 px-6 text-[#fff] border border-primaryColor rounded-[30px] w-fit"
-              onClick={() => navigate('/pricing')}
-            >
+              className='w-full md:w-auto flex  items-center justify-center gap-2 text-sm leading-6 bg-primaryColor py-2 px-6 text-[#fff] border border-primaryColor rounded-[30px] w-fit'
+              onClick={() => navigate('/pricing')}>
               Get Started
               <svg
-                width="14"
-                height="12"
-                viewBox="0 0 14 12"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
+                width='14'
+                height='12'
+                viewBox='0 0 14 12'
+                fill='none'
+                xmlns='http://www.w3.org/2000/svg'>
                 <path
-                  d="M12.8125 5.79297L1.5625 5.79297"
-                  stroke="white"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  d='M12.8125 5.79297L1.5625 5.79297'
+                  stroke='white'
+                  stroke-width='1.5'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
                 />
                 <path
-                  d="M8.27539 1.27575L12.8129 5.79375L8.27539 10.3125"
-                  stroke="white"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  d='M8.27539 1.27575L12.8129 5.79375L8.27539 10.3125'
+                  stroke='white'
+                  stroke-width='1.5'
+                  stroke-linecap='round'
+                  stroke-linejoin='round'
                 />
               </svg>
             </button>
-            <button
-              className="text-sm leading-6 bg-transparent py-2 px-6 text-[#fff] border border-white rounded-[30px] w-fit"
-              onClick={() => navigate('/available-players')}
-            >
-              Coaches
-            </button>
-            <button
-              className="text-sm leading-6 bg-transparent py-2 px-6 text-[#fff] border border-white rounded-[30px] w-fit"
-              onClick={() => navigate('/available-players')}
-            >
-              Players
-            </button>
+            <div className='flex items-center gap-2 flex-row w-full md:w-auto'>
+              <button
+                className='w-full md:w-auto text-sm leading-6 bg-transparent py-2 px-6 text-[#fff] border border-white rounded-[30px] w-fit'
+                onClick={() => navigate('/available-players')}>
+                Coaches
+              </button>
+              <button
+                className='w-full md:w-auto text-sm leading-6 bg-transparent py-2 px-6 text-[#fff] border border-white rounded-[30px] w-fit'
+                onClick={() => navigate('/available-players')}>
+                Players
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- src/components/PlayerProfile/PlayerOffers.jsx
+++ src/components/PlayerProfile/PlayerOffers.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 const PlayerOffers = ({ offers }) => {
   // Function to format date
@@ -11,55 +11,76 @@ const PlayerOffers = ({ offers }) => {
   return (
     <div>
       {/* wrapper */}
-      <div className="flex flex-col gap-4">
+      <div className='flex flex-col gap-4'>
         {offers && offers.length > 0 ? (
           offers.map((item, index) => (
             <div
-              className="flex items-center justify-between p-3 bg-[#fff] rounded-xl shadow-[0px_0px_13px_0px_rgba(0,0,0,0.05)]"
-              key={index}
-            >
+              className='flex items-center justify-between p-3 bg-[#fff] rounded-xl shadow-[0px_0px_13px_0px_rgba(0,0,0,0.05)]'
+              key={index}>
               {/* details area */}
-              <div className="flex items-center gap-2.5">
+              <div className='flex items-center gap-2.5'>
                 {/* icon */}
-                <div className="w-[45px] h-[45px] rounded-full bg-[#F1F1F1] p-2.5">
+                <div className='w-[45px] h-[45px] rounded-full bg-[#F1F1F1] p-2.5'>
                   <img
-                    className="w-full h-full object-contain"
+                    className='w-full h-full object-contain'
                     src={item?.logo}
-                    alt=""
+                    alt=''
                   />
                 </div>
 
                 <div>
-                  <p className="text-base font-medium text-[#000] leading-normal">
+                  <p className='text-base font-medium text-[#000] leading-normal'>
                     {item?.university}
                   </p>
-                  <p className="text-sm text-[#000] leading-normal">
-                    {item?.date ? formatDate(item?.date) : ""}
+                  <p className='text-sm text-[#000] leading-normal'>
+                    {item?.date ? formatDate(item?.date) : ''}
                   </p>
                 </div>
               </div>
 
               {/* status area */}
-              <div className="py-1 px-2.5 rounded-[40px] bg-[#F1F1F1] flex items-center gap-1">
-                <span
+              <div
+                className={`py-1 px-2.5 rounded-[40px]  flex items-center gap-1 border ${
+                  item?.status === 'Committed'
+                    ? 'border-green-500 bg-green-100'
+                    : item?.status === 'Offered'
+                    ? 'border-blue-500 bg-blue-100'
+                    : item?.status === 'Visited'
+                    ? 'border-yellow-300 bg-yellow-50'
+                    : item?.status === 'Visiting'
+                    ? 'border-purple-500 bg-purple-100'
+                    : item?.status === 'Walk-on'
+                    ? 'border-orange-500 bg-orange-100'
+                    : item?.status === 'Redshirt'
+                    ? 'border-black bg-gray-200'
+                    : item?.status === 'Interested'
+                    ? 'border-yellow-600 bg-yellow-200'
+                    : item?.status === 'Transferred'
+                    ? 'border-red-500 bg-red-100'
+                    : item?.status === 'Pending'
+                    ? 'border-white bg-gray-100'
+                    : item?.status === 'Signed'
+                    ? 'border-cyan-500 bg-cyan-100'
+                    : ''
+                }`}>
+                {/* <span
                   style={{
                     backgroundColor: `${
-                      item?.status?.toLowerCase() === "offered" ||
-                      item?.status?.toLowerCase() === "committed"
-                        ? "#1EF24D"
-                        : ""
+                      item?.status?.toLowerCase() === 'offered' ||
+                      item?.status?.toLowerCase() === 'committed'
+                        ? '#1EF24D'
+                        : ''
                     }`,
                   }}
-                  className="w-1 h-1 rounded-full bg-[#f33]"
-                ></span>
-                <p className="text-sm text-[#000] leading-normal ">
+                  className='w-1 h-1 rounded-full bg-[#f33]'></span> */}
+                <p className='text-sm text-[#000] leading-normal '>
                   {item?.status}
                 </p>
               </div>
             </div>
           ))
         ) : (
-          <p className="text-center text-[#000] text-base leading-6 font-normal">
+          <p className='text-center text-[#000] text-base leading-6 font-normal'>
             No offers
           </p>
         )}
